91752648069678ab6cefe8855cb4c312f7ec2b6f
Shiyinq <shiyinq.github.io@gmail.com>
2026-04-09 00:57:57 +0700

    feat: include scraper directory in linting and formatting scripts

--- scripts/lint-format.sh
+++ scripts/lint-format.sh
@@ -6,6 +6,6 @@ if [ -d ".venv" ]; then
     source .venv/bin/activate
 fi
 
-autoflake --remove-all-unused-imports --recursive --remove-unused-variables --in-place src --exclude=__init__.py,_example
-isort src --profile black
-black src
+autoflake --remove-all-unused-imports --recursive --remove-unused-variables --in-place src scraper --exclude=__init__.py,_example
+isort src scraper --profile black
+black src scraper
