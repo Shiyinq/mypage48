94f480e7da1f7037001541014802fde14e929725
Shiyinq <shiyinq.github.io@gmail.com>
2026-06-05 20:10:17 +0700

    refactor: improve readability of animated background transform calculation by wrapping long expressions

--- frontend/src/lib/components/common/AnimatedBackground.svelte
+++ frontend/src/lib/components/common/AnimatedBackground.svelte
@@ -107,7 +107,9 @@
 			style="
                 left: {d.x}%;
                 top: {d.y}%;
-                transform: scale({d.scale}) translate({($mouse.x || 0) * d.depth}px, {($mouse.y || 0) * d.depth +
+                transform: scale({d.scale}) translate({($mouse.x || 0) * d.depth}px, {($mouse.y ||
+				0) *
+				d.depth +
 				(scrollY || 0) * d.depth * 0.002}px);
             "
 		>
